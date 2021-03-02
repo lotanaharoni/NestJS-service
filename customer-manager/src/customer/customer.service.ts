a4d7b9b1a8bbf775c4baf8a0c424c77c8cda55db
lotanaharoni <50168804+lotanaharoni@users.noreply.github.com>
2021-03-02 23:15:33 +0200

    Update customer.service.ts

--- customer-manager/src/customer/customer.service.ts
+++ customer-manager/src/customer/customer.service.ts
@@ -6,6 +6,9 @@ import {ILike} from "typeorm";
 
 const MIN_LENGTH = 3;
 const ERROR = 404;
+const WRONG_FIRST_NAME_ERROR = 'You must enter a valid "firstName"';
+const SHORT_PARAM = 'The first name and the last name need to be more than 3 characters';
+const WRONG_PARAMS = 'firstName or lastName are not valid';
 
 @Injectable()
 export class CustomerService {
@@ -21,11 +24,11 @@ export class CustomerService {
     public async addCustomer(customer: CustomerEntity) {
         customer.lastName = customer.lastName? customer.lastName : "";
        if (!customer.firstName){
-        throw new HttpException('You must enter a valid "firstName"', ERROR);
+        throw new HttpException(WRONG_FIRST_NAME_ERROR, ERROR);
        }
        if (customer.firstName.length < MIN_LENGTH || 
         (customer.lastName.length > 0 && customer.lastName.length <MIN_LENGTH)){
-           throw new HttpException('The first name and the last name need to be more than 3 characters', ERROR);
+           throw new HttpException(SHORT_PARAM, ERROR);
         }
         else{
             return this.customerRepository.save(customer);
@@ -41,6 +44,6 @@ export class CustomerService {
             let result = await this.customerRepository.findOne({firstName: ILike(firstName), lastName: ILike(lastName)});
             return Boolean(result);
         }
-        throw new HttpException('firstName or lastName are not valid', ERROR);
+        throw new HttpException(WRONG_PARAMS, ERROR);
     }
 }   
